9de250c5cb6663b5670bb3ea553cdc670c6b12fd
Bryce Mara Jarrett <brycej98@gmail.com>
2020-11-02 19:20:52 -0600

    Add User Parent Class

---
+++ src/User.js
@@ -0,0 +1,20 @@
+class User {
+  constructor(roomData, bookingData) {
+
+  }
+  searchAvailibility() {
+
+  }
+  bookRoom() {
+
+    // call Post Data Function from fetchAPI object
+  }
+  cancelRoom() {
+
+  }
+  calculateTotalAmountSpent() {
+
+  }
+};
+
+export default User;
