0fb54db609293bb1822dc192d95e75d602c8568f
Bryce Mara Jarrett <brycej98@gmail.com>
2020-11-03 19:46:28 -0600

    Arrow functions

--- test/fetchAPI-test.js
+++ test/fetchAPI-test.js
@@ -12,35 +12,35 @@ describe('fetchApi', () => {
     chai.spy.on(fetchApi, ['fetchUserData', 'fetchRoomData', 'fetchBookingData', 'deleteBookingData', 'postBookingData'], () => {})
   });
 
-  it('should be able to fetch user data', function() {
+  it('should be able to fetch user data', () => {
     fetchApi.fetchUserData();
 
     expect(fetchApi.fetchUserData).to.have.been.called(1);
     expect(fetchApi.fetchUserData).to.have.been.called.with();
   });
 
-  it('should be able to fetch room data', function() {
+  it('should be able to fetch room data', () => {
     fetchApi.fetchRoomData();
 
     expect(fetchApi.fetchRoomData).to.have.been.called(1);
     expect(fetchApi.fetchRoomData).to.have.been.called.with();
   });
 
-  it('should be able to fetch user data', function() {
+  it('should be able to fetch user data', () => {
     fetchApi.fetchBookingData();
 
     expect(fetchApi.fetchBookingData).to.have.been.called(1);
     expect(fetchApi.fetchBookingData).to.have.been.called.with();
   });
 
-  it('should be able to delete booking data', function() {
+  it('should be able to delete booking data', () => {
     fetchApi.deleteBookingData();
 
     expect(fetchApi.deleteBookingData).to.have.been.called(1);
     expect(fetchApi.deleteBookingData).to.have.been.called.with();
   });
 
-  it('should be able to post booking data', function() {
+  it('should be able to post booking data', () => {
     fetchApi.postBookingData();
 
     expect(fetchApi.postBookingData).to.have.been.called(1);
