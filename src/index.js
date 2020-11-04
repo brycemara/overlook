65fc6c63f9edd7f0c226a3ecb24ea3cd7c37ef3b
Bryce Mara Jarrett <brycej98@gmail.com>
2020-11-04 12:14:04 -0600

    Login functionality & class instantiations

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/hotel-california.jpeg'
 
+import User from './User';
+import Customer from './Customer';
+import Manager from './Manager';
 
 import {fetchApi} from './fetchAPI'
-
-
-
-const fetchedUserData = fetchApi.fetchUserData();
-const fetchedRoomData = fetchApi.fetchRoomData();
-const fetchedBookingData = fetchApi.fetchBookingData();
 
 let userData;
 let roomData;
 let bookingData;
 
 function reAssignData() {
+  const fetchedUserData = fetchApi.fetchUserData();
+  const fetchedRoomData = fetchApi.fetchRoomData();
+  const fetchedBookingData = fetchApi.fetchBookingData();
   Promise.all([fetchedUserData, fetchedRoomData, fetchedBookingData]).then(values => {
     userData = values[0];
     roomData = values[1];
@@ -33,29 +33,30 @@ loginButton.addEventListener('click', checkLogin)
 
 function checkLogin() {
   if (userNameInput.value === 'manager' && passwordInput.value === 'overlook2020') {
+    instantiateManager();
     managerDisplayLogin();
+  } else if (userNameInput.value.includes('customer') && passwordInput.value === 'overlook2020') {
+    let splitUserName = userNameInput.value.split('customer');
+    let userID = parseInt(splitUserName[1]);
+    instantiateCustomer(userID);
+    customerDisplayLogin();
   }
-// if its a manager display manager & instantiate a manager
-// if its a customer display customer & instantiate a customer
-}
-
-function getRandomCustomerIndex() {
-  const userIndex = Math.floor(Math.random() * 50);
-  return userData[userIndex]
 }
 
 function customerDisplayLogin() {
-
+  document.querySelector('.login-form').classList.add('hidden');
+  document.querySelector('.customer-dashboard').classList.remove('hidden');
 }
 
-function instantiateCustomer() {
-  const customer = new Customer(roomData, bookingData, userData, getRandomUserIndex());
+function instantiateCustomer(userID) {
+  let user = userData.find(user => user.id === userID)
+  const customer = new Customer(roomData, bookingData, userData, user);
   return customer;
 }
 
 function managerDisplayLogin() {
   document.querySelector('.login-form').classList.add('hidden');
-  document.querySelector('.manager-dashboard').classList.remove('hidden')
+  document.querySelector('.manager-dashboard').classList.remove('hidden');
 }
 
 function instantiateManager() {
