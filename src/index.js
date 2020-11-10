8b8eb3848c03945a7b02df5157b52fc07b7a09d1
Bryce Mara Jarrett <brycej98@gmail.com>
2020-11-10 09:56:00 -0600

    Add search avaible room for todays date

--- src/index.js
+++ src/index.js
@@ -130,18 +130,25 @@ function bookARoom(e) {
 
   }
   checkWhatToBook(roomNumber, userID, onCustomerSuccess, onManagerSuccess)
-  alert("You've successfuly booked a room!")
+  // alert("You've successfuly booked a room!")
 }
 
 function checkWhatToBook(roomNumber, userID, onCustomerSuccess, onManagerSuccess) {
   if(!manager) {
     let date = document.getElementById('date-input').value;
     let formattedDate = date.split(/[-]+/).join('/');
+    if(compareDates(date)) {
+      alert("You cannot cancel a booking from the past");
+      return;
+    }
     fetchApi.postBookingData(roomNumber, userID, formattedDate, onCustomerSuccess);
+    alert("You've successfuly booked a room!")
   } else {
     let date = document.getElementById('avaiable-rooms-date').value;
     let formattedDate = date.split(/[-]+/).join('/');
+
     fetchApi.postBookingData(roomNumber, userID, formattedDate, onManagerSuccess);
+    alert("You've successfuly booked a room!")
   }
 }
 
@@ -284,6 +291,11 @@ function displayAvaiableRooms() {
   let user = getCusomterInfo('customer-name-avaiable');
   customer = new Customer(roomData, bookingData, userData, user)
   let date = document.getElementById('avaiable-rooms-date').value;
+  let formattedDate = date.split(/[-]+/).join('/');
+  if(compareDates(formattedDate)) {
+    alert("You cannot cancel a booking from the past");
+    return;
+  }
   let avaibleRooms = manager.searchAvailibility(date);
   avaibleRooms.forEach((room) => {
     document.querySelector('.avaiable-results').insertAdjacentHTML('beforeend', domUpdates.createRoomBlocks(room, date));
